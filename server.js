ee3eca485ea0771c6d2a9e69b6db9682de88ae67
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-24 10:32:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(userRoutes);
+//Home page route
+app.get("/", (request, response) => {
+  response.send("Welcome to TheMealDB: An open, crowd-sourced database of Recipes from around the world.");
+});
 
 app.listen(process.env.PORT || 3002, () => {
   console.log(`server running in port 3002`);
